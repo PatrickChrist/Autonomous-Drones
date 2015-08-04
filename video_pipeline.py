fe561953de0e6b4a16c8a2378f6b3a7292bfff0a
Benedikt Brandner <ga49xel@mytum.de>
2015-08-04 20:31:40 +0200

    interface sample

--- video_pipeline.py
+++ video_pipeline.py
@@ -2,6 +2,7 @@ __author__ = 'Benedikt'
 
 import cv2
 import autonomous_flight
+from main import interface
 
 class Pipeline:
     def __init__(self, drone):
@@ -11,6 +12,7 @@ class Pipeline:
     def on_frame(self):
         af = autonomous_flight.AF(0,self.cv_win,self.drone)
         af.run()
+        # to steer: interface.steer_autonomous("up")
         
         
 if __name__ == '__main__':

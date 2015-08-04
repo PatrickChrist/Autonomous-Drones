8b42a97e4b1ff00c678d928d01328acf1688a023
Benedikt Brandner <ga49xel@mytum.de>
2015-08-04 20:50:25 +0200

    pipeline ctd.

--- keyboard_control.py
+++ keyboard_control.py
@@ -12,7 +12,61 @@ class KeyboardControl:
         :returns truth value whether to continue running
     """
     def on_key(self, key):
-        pass
+        if key == 27:  # esc
+            interface.steer_manual("land")
+            return False
+
+        if key == ord('p'):
+            interface.steer_manual("reset")
+
+        if not flying:
+            if key == ord(' '):
+                drone.takeoff()
+                drone.hover()
+                flying = True
+        else:
+            if key == ord(' '):
+                drone.hover()
+                drone.land()
+                flying = False
+            elif key == ord('w'):
+                return_to_hover = True
+                drone.move_forward()
+            elif key == ord('a'):
+                return_to_hover = True
+                drone.move_left()
+            elif key == ord('s'):
+                return_to_hover = True
+                drone.move_backward()
+            elif key == ord('d'):
+                return_to_hover = True
+                drone.move_right()
+            elif key == ord('q'):
+                return_to_hover = True
+                drone.turn_left()
+            elif key == ord('e'):
+                return_to_hover = True
+                drone.turn_right()
+            elif key == 2490368:  # up
+                return_to_hover = True
+                drone.move_up()
+            elif key == 2621440:  # down
+                return_to_hover = True
+                drone.move_down()
+            elif key == ord('1'):
+                drone.speed = 0.1
+            elif key == ord('2'):
+                drone.speed = 0.3
+            elif key == ord('3'):
+                drone.speed = 0.5
+            elif key == ord('4'):
+                drone.speed = 0.9
+            else:
+                if return_to_hover:
+                    return_to_hover = False
+                    drone.hover()
+
+            return True
 
     def manual_controlled(self):
         return self.manual_control
--- main.py
+++ main.py
@@ -7,26 +7,68 @@ import keyboard_control
 import video_pipeline
 
 
-class Tower:
+class Tower:  # controlls the drone movements
     def __init__(self, drone):
         self.drone = drone
         self.manual_steering = False
+        self.flying = False
 
-    def steer(self, command):
-        pass  # TODO
+    def set_manual_mode(self, value):
+        self.manual_steering = value
 
-    def steer_autonomous(self, command):
+    def steer(self, command, speed=0.3):
+        self.drone.speed = speed
+        if command == "up":
+            self.drone.move_up()
+        if command == "down":
+            self.drone.move_down()
+        if command == "left":
+            self.drone.move_left()
+        if command == "right":
+            self.drone.move_right()
+        if command == "turnleft":
+            self.drone.turn_left()
+        if command == "turnright":
+            self.drone.turn_right()
+        if command == "forward":
+            self.drone.move_forward()
+        if command == "backward":
+            self.drone.move_backward()
+        if command == "takeoff":
+            if self.flying:
+                return
+            else:
+                self.flying = True
+                self.drone.takeoff()
+        if command == "land":
+            if self.flying:
+                self.flying = False
+                self.drone.land()
+            else:
+                return
+        if command == "reset":
+            self.flying = False
+            self.drone.reset()
+        if command == "hover":
+            self.drone.hover()
+
+    def steer_manual(self, command, speed=0.3):
+        self.set_manual_mode(True)
+        self.steer(command, speed)
+
+    def steer_autonomous(self, command, speed=0.3):
         if self.manual_steering:
             print "disregarding input while in manual mode"
-            return
-        self.steer(command)
+        else:
+            self.steer(command, speed)
 
 
 def main_loop():
     running = True
     while running:
         key = cv2.waitKey(33)
-        keyboard.on_key(key)
+        running = keyboard.on_key(key)
+
 
 
 def init():

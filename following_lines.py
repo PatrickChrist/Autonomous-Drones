82ba64b017a914fa5e31931f2174b485c9c27df1
broen <broen@dhcp-10-180-25-208.dynamic.eduroam.mwn.de>
2015-08-06 15:29:05 +0200

    key controls

--- following_lines.py
+++ following_lines.py
@@ -12,6 +12,7 @@ import numpy as np
 #variables
 running = True
 flying = False
+line_in_sight = False
 # operational
 speed = 0.1 #forward movement
 gain_x = 0.1 #horizontal gain
@@ -21,6 +22,9 @@ gain_r = 0.1 #rotation gain
 optimum_x = 160 #center in the middle of 320px frame
 optimum_z = 800 #keep height at 80cm
 optimum_r = 0 #no rotation por favor
+#image buffer
+buffer_counter = 0
+buffer_distance = 3
 
 # init da drone
 drone=libardrone.ARDrone(1,1) #initalize the Drone Object
@@ -30,7 +34,9 @@ while running:
     try:
 # get image
         pixelarray = drone.get_image() # get an frame form the Drone
-        if pixelarray != None: # check whether the frame is not empty
+        buffer_counter += 1
+        # check whether the frame is not empty, only take every nth picture
+        if pixelarray != None and buffer_counter % buffer_distance == 0: 
             frame = pixelarray[:, :, ::-1].copy() #convert to a frame
             resized = cv2.resize(frame, (320, 180)) #resize image
             # display the image
@@ -54,10 +60,9 @@ while running:
             #find lines
             lines = cv2.HoughLines(edges,1,np.pi/180,50)
 # decisions
-            if lines is None:
-                #TODO search for the line
-                print 'no line in sight'
-            else:
+            if lines is not None:
+#TODO search for the line
+            
                 for rho,theta in lines[0]:
                     a = np.cos(theta)
                     b = np.sin(theta)
@@ -67,9 +72,9 @@ while running:
                     z = drone.navdata[0]['altitude']
 # corrections
                     #check speed maybe?                    
-                    correct_x = (optimum_x - x) / optimum_x * gain_x
+                    correct_x = (x - optimum_x) / optimum_x * gain_x
                     correct_z = (optimum_z - z) / optimum_z * gain_z
-                    correct_r = (optimum_r - slope) * gain_r
+                    correct_r = (slope - optimum_r) * gain_r
 # print to console
                     
                     print_x = 'right   ' if correct_x > 0 else 'left    '
@@ -78,7 +83,7 @@ while running:
                     print print_x + print_z + print_r
                     #print 'speed:', speed, 'x:', correct_x, 'up:', correct_z, 'rotate:', correct_r
 #call the drone
-                    drone.at(drone.at_pcmd, correct_x, speed, correct_z, correct_r)
+                    drone.at(drone.at_pcmd, correct_x, speed, 0, correct_r)
 #keyboard controls
             k = cv2.waitKey(33)
             if k == 27: #stop with esc
@@ -89,6 +94,33 @@ while running:
                 else:
                     drone.takeoff()
                 flying = not flying
+                # wasd for front/back/left/right
+            elif k == ord('a'):
+                drone.move_left()
+            elif k == ord('d'):
+                drone.move_right()
+            elif k == ord('w'):
+                drone.move_forward()
+            elif k == ord('s'):
+                drone.move_back()
+                
+            # arrow keys for up/down/rotate
+            elif k == 63232:
+                drone.move_up
+            elif k == 63233:
+                drone.move_down()
+            elif k == 63234:
+                drone.turn_left()
+            elif k == 63235:
+                drone.turn_right()
+            
+            #hover on h
+            elif k == ord('h'):
+                drone.hover()
+            #speed adjustment on i & o
+            elif k == ord('i'):
+                speed += 0.1
+            elif k == ord('o'):
+                speed -= 0.1
     except:
         print ""
-        

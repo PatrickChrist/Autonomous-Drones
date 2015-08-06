139c4f2db3ca4fdb3f4d8a6ab27a3b22de91b421
David Adrian <dawidh.adrian@googlemail.com>
2015-08-06 07:16:47 +0200

    fixed move instructions by explicit float cast

--- dev/rainbow_dash.py
+++ dev/rainbow_dash.py
@@ -12,7 +12,7 @@ import numpy as np
 
 # FUCKING GLOBALS YEE
 pattern_size = (4, 5)
-distance_best = [65, 85]
+distance_best = [75, 95]
 
 ##FLIGHT MODE###
 
@@ -24,13 +24,16 @@ class PID:
         self.Ki = Ki
         self.last_err_x = 0
         self.last_err_y = 0 
+        self.last_err_dist = 0
     
-    def action(self, err_x, err_y):
+    def action(self, err_x, err_y, err_dist):
         out_x = self.Kp*err_x + self.Ki*(err_x+self.last_err_x) + self.Kd*(err_x-self.last_err_x)
         out_y = self.Kp*err_y + self.Ki*(err_y+self.last_err_y) + self.Kd*(err_y-self.last_err_y)
+        out_dist = self.Kp*err_dist + self.Ki*(err_dist+self.last_err_dist) + self.Kd*(err_dist-self.last_err_dist)
         self.last_err_x = err_x
         self.last_err_y = err_y
-        return out_x, out_y
+        self.last_err_dist = err_dist
+        return out_x, out_y, out_dist
     
 def get_main_corners_from_corners(npcorners):
     return np.array([npcorners[0][0],npcorners[3][0],npcorners[16][0],npcorners[19][0]])
@@ -68,6 +71,8 @@ def start_up_drone():
     
     drone.startup()									# Connects to drone and starts subprocesses
     drone.reset()										# Always good, at start
+    
+    drone.useDemoMode(True)
     
     while drone.getBattery()[0] == -1:	time.sleep(0.1)		      # Waits until the drone has done its reset
     time.sleep(0.5)									# Give it some time to fully awake
@@ -107,23 +112,29 @@ def flush_capture_stream(cam):
     t0 = 0
     t1 = 0
     #print doing flush
-    while delay < 15:
+    while delay < 25:
         t0 = time.time()
-        cam.grab()
+        cam.read()
         t1 = time.time()
         delay = (t1 - t0) * 1000
         
 def main():
     drone = start_up_drone()
     drone.sdVideo()             
-    drone.videoFPS(20)                      
-    drone.frontCam()                                             # Choose front view
+    #drone.videoFPS(30)              
+    drone.fastVideo()      
+    drone.mp4Video()
+    drone.videoBitrate(250)
+    drone.frontCam()           
+    CDC = drone.ConfigDataCount
+    while CDC==drone.ConfigDataCount: time.sleep(0.001)
+    drone.startVideo()
 
     drone.trim()
-    cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
+    #cam = cv2.VideoCapture('tcp://192.168.1.1:5555')
     
     #cam = cv2.VideoCapture(0)
-    pid = PID(0.5,1.0,0.15)
+    pid = PID(0.3,0.8,0.05)
     print "Created VideoCapture"
     
 #    #debug video boot sequence
@@ -136,13 +147,26 @@ def main():
 #    
     running = True
     do_flush = False
+    IMC = drone.VideoImageCount
+    
+    v_f = 0.05
+    v_r = 0.12
+    v_h = 0.01
+    t_f = 0.15
+    t_r = 0.05    
+    t_h = 0.2
+    
     while running:    # get current frame of video   
-        running, frame = cam.read() 
+        #running, frame = cam.read() 
         running = manual_control(drone, running)
-        
-        if do_flush:
-            #flush_capture_stream(cam)
-            do_flush = False
+
+        while drone.VideoImageCount==IMC:   time.sleep(0.01)
+            
+        frame = drone.VideoImage
+#        
+#        if do_flush:
+#            #flush_capture_stream(cam)
+#            do_flush = False
         
         if running:    
             found, corners = get_corners_from_marker(frame)
@@ -151,32 +175,43 @@ def main():
                 o_corners = get_main_corners_from_corners(corners)
                 height, width = frame.shape[:2]
                 errx, erry = get_centroid_error(centroid, width, height)
-                out_x,out_y = pid.action(errx,erry)
-               # print erry, out_y
-                
                 errdif = get_distance_error(o_corners)
-#                if errx > 0:
-#                    drone.move(0.0, 0.0, 0.0, 0.1)     
-#                else:
-#                    drone.move(0.0, 0.0, 0.0, -0.1) 
-                if errdif > 0.1 and errx > 0.1:
-                    drone.move(0.0, -0.1, 0.0, 0.1)     
-                elif errdif < -0.1 and errx > 0.1:
-                    drone.move(0.0, 0.1, 0.0, 0.1) 
-                elif errdif > 0.1 and errx < -0.1:
-                    drone.move(0.0, -0.1, 0.0, -0.1) 
-                elif errdif < -0.1 and errx < -0.1:
-                    drone.move(0.0, 0.1, 0.0, -0.1) 
-                elif errdif < -0.1 and abs(errx) < 0.1:
-                    drone.move(0.0, 0.1, 0.0, 0.0)
-                elif errdif > 0.1 and abs(errx) < 0.1:
-                    drone.move(0.0, -0.1, 0.0, 0.0)
-                elif abs(errdif) < 0.1 and errx > 0.1:
-                    drone.move(0.0, 0.0, 0.0, 0.1)
-                elif abs(errdif) < 0.1 and errx < -0.1:
-                    drone.move(0.0, 0.0, 0.0, -0.1)                     
+                ox, oy, odist = pid.action(errx,erry,errdif)
+                
+#                fv = 0.0
+#                hv = 0.0
+#                rv = 0.0
+#                if(abs(errx) > t_r):
+#                    rv = np.sign(errx) * v_r
+#                if(abs(erry) > t_h):
+#                    hv = np.sign(erry) * v_h
+#                if(abs(errdif) > t_f):
+#                    fv = np.sign(errdif) * v_f
                     
-                #draw inner circle
+                #print rv, hv, fv
+                #drone.move(0.0,-float(fv),-float(hv),float(rv))
+                print ox, oy, odist
+                drone.move(0.0,-0.2*float(odist),-0.5*float(oy),float(ox))
+                
+
+#                if errdif > 0.1 and errx > 0.05:
+#                    drone.move(0.0, -0.05, 0.0, 0.2)     
+#                elif errdif < -0.1 and errx > 0.05:
+#                    drone.move(0.0, 0.11, 0.0, 0.2) 
+#                elif errdif > 0.1 and errx < -0.05:
+#                    drone.move(0.0, -0.05, 0.0, -0.2) 
+#                elif errdif < -0.1 and errx < -0.05:
+#                    drone.move(0.0, 0.11, 0.0, -0.2) 
+#                elif errdif < -0.1 and abs(errx) < 0.05:
+#                    drone.move(0.0, 0.11, 0.0, 0.0)
+#                elif errdif > 0.1 and abs(errx) < 0.05:
+#                    drone.move(0.0, -0.05, 0.0, 0.0)
+#                elif abs(errdif) < 0.1 and errx > 0.05:
+#                    drone.move(0.0, 0.0, 0.0, 0.2)
+#                elif abs(errdif) < 0.1 and errx < -0.05 :
+#                    drone.move(0.0, 0.0, 0.0, -0.2)                     
+
+                    
                 cv2.circle(frame, (centroid[0][0],centroid[0][1]), 10,(255,0,0) ,1)
                 cv2.line(frame, (o_corners[0][0],o_corners[0][1]), (o_corners[1][0],o_corners[1][1]), (0,255,0),1)
                 cv2.line(frame, (o_corners[1][0],o_corners[1][1]), (o_corners[3][0],o_corners[3][1]), (0,255,255),1)
@@ -186,7 +221,7 @@ def main():
             if not found:
                 print 'chessboard not found'
                 drone.hover()
-                do_flush = True
+                #do_flush = True
                 continue
             
             cv2.imshow('frame', frame)  
@@ -196,7 +231,7 @@ def main():
             else:        # error reading frame        
                 print 'error reading video feed'
       
-    cam.release()
+    #,cam.release()
     cv2.destroyAllWindows()
     
 if __name__ == "__main__":

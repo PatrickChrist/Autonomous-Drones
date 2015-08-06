c74464d6d389b02fd2e124ab59bfb6b1c9db157c
broen <broen@dhcp-10-180-25-208.dynamic.eduroam.mwn.de>
2015-08-06 23:36:40 +0200

    rho und so

--- following_lines_noturn.py
+++ following_lines_noturn.py
@@ -9,6 +9,7 @@ import libardrone.libardrone as libardrone
 import cv2
 import numpy as np
 import time
+import math
 
 
 #variables
@@ -17,23 +18,40 @@ flying = False
 loop = False
 line_in_sight = False
 # operational
-speed = 0 #forward movement
+speed_ratio = 40 #speed 1/n
+angle_change = 0.005 #constant to navigate towards the line on x-axis
 gain_x = 0.3 #horizontal gain
 gain_z = 0.3 #vertical_gain
 # optimum
 optimum_x = 160 #center in the middle of 320px frame
-optimum_z = 800 #keep height at 80cm
+optimum_z = 650 #keep height at 80cm
 #image buffer
 buffer_counter = 0
 buffer_distance = 10
 buffer_image = None
 
+hover_timer = time.time()
+
 # init da drone
 drone=libardrone.ARDrone(1,1) #initalize the Drone Object
 print 'battery:', drone.navdata[0]['battery']
 
-
-buffer_time = time.time()
+# calculate the center of the line
+def middle(start, end, a, b):
+    x0, y0 = start
+    xn, yn = end
+    in_range = None
+    num_in_range = 0
+    while x0 < xn and y0 < yn:
+        x0 += a
+        y0 += b
+        if x0 >= 0 and y0 >= 0 and x0 <= 320 and y0 <= 180:
+            if in_range == None:
+                in_range = (x0, y0)
+            num_in_range += 1
+    xl, yl = in_range
+    return (xl + (-b) * (num_in_range / 2), yl + a * (num_in_range / 2))
+        
 while running:
     try:
 # get image
@@ -74,37 +92,59 @@ while running:
             if lines is not None and loop:
 #TODO search for the line
             
-                for rho,theta in lines[0]:
-                    a = np.cos(theta)
-                    b = np.sin(theta)
+                if time.time() - hover_timer < 5:
+                    for rho,theta in lines[0]:
                     
-                    if theta < math.pi / 2:
-                        b = -b
+                        a = np.cos(theta)
+                        b = np.sin(theta)
+                        x = a * rho
+                        
+                        x = middle((x - 400 * b, y + 400 * a), (x + 400 * b, y - 400 * a), a, b)
+                        # fake theta
+                        theta_delta = 0
+                        if x < optimum_x - 20:
+                            theta += angle_change
+                            theta_delta = -angle_change
+                        elif x > optimum_x + 20:
+                            theta += angle_change
+                            theta_delta = angle_change
+                        a = np.cos(theta)
+                        b = np.sin(theta)
+                        
+                        if theta < math.pi / 2:
+                            b = -b
+                    
+                        w, h, d = frame.shape
                 
-                    w, h, d = frame.shape
-            
-                    cv2.line(resized, (int(w/2), int(h/2)), (int(w/2+b*1000), int(h/2-a*1000)), (255, 0, 255))
-                    lr = str(b / 3)
-                    rb = str(-a / 3)
-                    font = cv2.FONT_HERSHEY_SIMPLEX
-                    cv2.putText(resized,'lr: ' + lr,(10,70), font, 1,(255,255,255),2)
-                    cv2.putText(resized,'rb: ' + rb,(10,120), font, 1,(255,255,255),2)
-# inputs
-                    x = a * rho
-                    slope = b / a
-                    z = drone.navdata[0]['altitude']
-# corrections
-                    #check speed maybe?                    
-                    correct_x = (x - optimum_x) / optimum_x * gain_x
-                    correct_z = (optimum_z - z) / optimum_z * gain_z
-# print to console
-                    
-                    print_x = 'right   ' if correct_x > 0 else 'left    '
-                    print_z = 'up    ' if correct_z > 0 else 'down  '
-                    print print_x, correct_x, print_z, correct_z
-                    #rint 'speed:', speed, 'x:', correct_x, 'up:', correct_z, 'rotate:', correct_r
-#call the drone
-                    drone.at(libardrone.at_pcmd, True, float(lr), -speed, 0, 0)
+                        #cv2.line(resized, (int(w/2), int(h/2)), (int(w/2+b*1000), int(h/2-a*1000)), (255, 0, 255))
+                        lr = str(b / speed_ratio)
+                        rb = str(abs(a) / speed_ratio)
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        #cv2.putText(resized,'lr: ' + lr,(10,70), font, 1,(255,255,255),2)
+                        #cv2.putText(resized,'rb: ' + rb,(10,120), font, 1,(255,255,255),2)
+                        #cv2.putText(resized,'th: ' + str(theta_delta),(10,170), font, 1,(255,255,255),2)
+                        resized = cv2.line(resized,(x,0),(x,180),(100,200,170),5)
+    # inputs
+                        
+                        slope = b / a
+                        z = drone.navdata[0]['altitude']
+    # corrections
+                        #check speed maybe?                    
+                        correct_x = (x - optimum_x) / optimum_x * gain_x
+                        
+                        correct_z = (optimum_z - z) / optimum_z * gain_z
+    # print to console
+                        
+                        print_x = 'right   ' if correct_x > 0 else 'left    '
+                        print_z = 'up    ' if correct_z > 0 else 'down  '
+                        print print_x, correct_x, print_z, correct_z
+                        #rint 'speed:', speed, 'x:', correct_x, 'up:', correct_z, 'rotate:', correct_r
+    #call the drone
+                        drone.at(libardrone.at_pcmd, True, -float(lr), -float(rb), correct_z, 0)
+                else:
+                    drone.hover()
+                    hover_timer = time.time()
+                    time.sleep(1)
 #keyboard controls
                     
             cv2.imshow('Drone', resized) # show the frame                    
@@ -118,7 +158,7 @@ while running:
                 else:
                     drone.takeoff()
                 flying = not flying
-                # wasd for front/back/left/right
+                # wasd for front/back/left/right 
             elif k == ord('a'): 
                 drone.move_left()
             elif k == ord('d'):
@@ -151,8 +191,12 @@ while running:
             elif k == ord('o'):
                 speed -= 0.05
                 print 'speed:', -speed
-            # start with p
-            elif k == ord('p'):
+            # start with l
+            elif k == ord('l'):
                 loop = True 
+                hover_timer = time.time()
+            # print height with h
+            elif k == ord('p'):
+                print drone.navdata[0]['altitude'] 
     except ValueError as e:
         print e

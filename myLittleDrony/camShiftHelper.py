f6cbf902a5100b8c9bac19073e7a8ff5673c2722
Maximilian Strobel <maximilian.strobel@tum.de>
2015-08-29 20:42:16 +0200

    cleanup myLittleDrony

--- myLittleDrony/camShiftHelper.py
+++ myLittleDrony/camShiftHelper.py
@@ -1,41 +1,55 @@
 import numpy as np
 import cv2
 
-usesOpenCV3 = False
+MIN_AREA = 400
+MAX_AREA = 57000
+
+useOpenCV3 = None
+follow_light = None
 
 def boxPoints(ret):
-	if (usesOpenCV3 == True):
+	if (useOpenCV3 == True):
 		return cv2.boxPoints(ret)
 	else:
 		return cv2.cv.BoxPoints(ret)
 
-def filterHSV(hsv_img):
-	hsv_img_filtered = hsv_img
-
 class CamShift(object):
 	def get_current_histogram_density(self, ret, pts):
+		#cut only the part within the current tracking area
 		mask = np.zeros(self.dst.shape, np.uint8)
 		cv2.fillPoly(mask, [pts], (255, 255, 255))
 		mask = mask/255
 		masked_dst = np.multiply(self.dst,mask)
 		tmp_dst = np.array(masked_dst)
+		
+		#get the sum of all pixel within the current tracking area
 		denstity_mask_sum = masked_dst.sum()
+		
+		#get the density of the current tracking area
   		density = int(denstity_mask_sum/(ret[1][0]*ret[1][1]))
-		#cv2.putText(tmp_dst, "Current Density", (10, 70), font, 1, (255,255,255),2, cv2.Line_AA)
+		
+		#write debug info on image
 		cv2.putText(tmp_dst, "Current Density: " + str(density), (10,120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
   		if self.initial_histogram_density != None:
   			cv2.putText(tmp_dst, "Original Density: " + str(self.initial_histogram_density), (10,70), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+			
+		# apply correction factor
 		if self.initialArea != None:
 			ratioCurrentToInitialArea = (ret[1][0]*ret[1][1]) / self.initialArea
 			if ratioCurrentToInitialArea < 1:
 				density = density *(1+ 1.5*(1-ratioCurrentToInitialArea)) #correction factor: if the square is farther away, the density usually decreases 
-		cv2.imshow("masked dst",tmp_dst)
-
-
+		
+		cv2.imshow("masked histogram",tmp_dst)
 		return density
 
-	def __init__(self,c,r,w,h,initialCamImage):
-
+	def __init__(self,c,r,w,h,initialCamImage,tmp_follow_light,tmp_useOpenCV3):
+		global useOpenCV3
+		useOpenCV3 = tmp_useOpenCV3
+		
+        # follow a relatively bright object or a object with a certain color
+		global follow_light
+		follow_light = tmp_follow_light
+             
 		# take first frame of the video
 		self.frame = initialCamImage
 
@@ -46,7 +60,12 @@ class CamShift(object):
 		self.roi = self.frame[r:r+h, c:c+w]
 		self.hsv_roi =  cv2.cvtColor(self.roi, cv2.COLOR_BGR2HSV)
 		self.mask = cv2.inRange(self.hsv_roi, np.array((0., 60.,32.)), np.array((180.,256.,256.)))
-		self.roi_hist = cv2.calcHist([self.hsv_roi],[2],self.mask,[256],[0,256])
+		
+		if follow_light:
+			self.roi_hist = cv2.calcHist([self.hsv_roi],[2],self.mask,[256],[0,256])
+		else:
+			self.roi_hist = cv2.calcHist([self.hsv_roi],[0],self.mask,[180],[0,180])
+		
 		cv2.normalize(self.roi_hist,self.roi_hist,0,255,cv2.NORM_MINMAX)
 
 		# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
@@ -66,9 +85,12 @@ class CamShift(object):
 	def performCamShift(self, camImage):
 
 		if camImage != None and camImage != []:
-			self.hsv = cv2.cvtColor(camImage, cv2.COLOR_BGR2HSV)
-			self.dst = cv2.calcBackProject([self.hsv],[2],self.roi_hist,[0,256],1)
-   			#self.dst = cv2.calcBackProject([self.hsv],[0,1],self.roi_hist,[180.0,256.0],[140.0,170.0,50.0,256.0],1.0)
+			self.hsvImage = cv2.cvtColor(camImage, cv2.COLOR_BGR2HSV)
+			if follow_light:
+				self.dst = cv2.calcBackProject([self.hsvImage],[2],self.roi_hist,[0,256],1)
+			else:
+				#follow color
+				self.dst = cv2.calcBackProject([self.hsvImage],[0],self.roi_hist,[0,180],1)
 
 			cv2.imshow("histo",self.dst)
 
@@ -77,7 +99,7 @@ class CamShift(object):
 			tmp_pts = boxPoints(tmp_ret)
 			tmp_pts = np.int0(tmp_pts)
 
-			# calc density
+			# calculate density and with/length ratio of current tracking window
 			if self.isFirstTime:
 				self.initial_histogram_density = self.get_current_histogram_density(tmp_ret, tmp_pts)
 				self.initialArea = tmp_ret[1][0]*tmp_ret[1][1]
@@ -90,17 +112,23 @@ class CamShift(object):
 					current_length_width_ratio = tmp_ret[1][0]/float(tmp_ret[1][1])
 				else:
 					current_length_width_ratio = 0
+			
+			# try to determine if camshift lost our object
 			area =(tmp_ret[1][0]*tmp_ret[1][1])
-			if area > 400 and area < 57000:# self.initial_histogram_density*0.2 < current_histogramm_density and area > 2500:# and current_length_width_ratio > 0.5 and current_length_width_ratio < 2:# and ratio_lastWH_and_currentWH > 0.5 and ratio_lastWH_and_currentWH < 2:
+			if area > MIN_AREA and area < MAX_AREA:# self.initial_histogram_density*0.2 < current_histogramm_density and area > 2500:# and current_length_width_ratio > 0.5 and current_length_width_ratio < 2:# and ratio_lastWH_and_currentWH > 0.5 and ratio_lastWH_and_currentWH < 2:
+				# decision criteria can be made stricter by commenting in the other conditions
+				
 				self.ret = tmp_ret
 				self.track_window = tmp_track_window
 				self.pts = tmp_pts
 				self.border_color = (0,255,0)
 				success = True
 			else:
-				self.border_color = (255,0,0)
+				# discard tmp_pts
+				self.border_color = (255,0,0) # used to print old tracking window in different color
 				success = False
 
+			
 			if self.pts != None:
 				self.img2 = camImage.copy()
 
@@ -112,9 +140,8 @@ class CamShift(object):
 				self.middleY = int(self.ret[0][1])
 				cv2.circle(self.img2,(self.middleX, self.middleY), 2, (10,255,255))
 				return success, self.img2, self.middleX, self.middleY, min(self.ret[1][0], self.ret[1][1])
-			
 			else:
-				return camImage, 0, 0, 0
+				return False, camImage, 0, 0, 0
 
 		else:
 			print 'No cam Image'
